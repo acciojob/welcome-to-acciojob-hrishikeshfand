8b042b16556fbab06ef8a65ae250cc8ae6dbc534
Hrishikesh Fand <hrishikeshfandn4111@gmail.com>
2025-11-02 12:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// Display a welcome alert when the page loads
+window.onload = function() {
+  alert("Welcome to Accio Jobs!");
+};
+
+// Optional: Log a message in the console
+console.log("Accio Jobs page loaded successfully!");
